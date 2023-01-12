725e525911d0922d35a5de0897545dfc87eef34c
jdonic <jdoni@jdonic.fedora>
2023-01-13 00:39:15 +0100

    Automate tests and linters
    Automate tests and linters using tox.ini file and setup
    Add an non existing App as a placeholder

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Movies app",
+    version="1.0.0",
+    author="Jakub Donič",
+    author_email="donicjakub@gmail.com",
+    description=("A Django app for scraping movies and actors and searching over them."),
+    url="https://github.com/jdonic/Movies",
+)
