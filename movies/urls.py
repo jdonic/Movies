419971f9386ce948cbdf8742e9408460a970f9cb
jdonic <jdoni@jdonic.fedora>
2023-01-13 13:17:19 +0100

    Create actor_detail
    Create url, view and basic template for
    actor_detail

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,9 @@
 # movies/urls.py
 from django.urls import path
-from .views import MovieDetailView
+from .views import MovieDetailView, ActorDetailView
 
 
 urlpatterns = [
     path("movie_detail/<int:pk>/", MovieDetailView.as_view(), name="movie_detail"),
+    path("actor_detail/<int:pk>/", ActorDetailView.as_view(), name="actor_detail"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,13 @@
 from django.views.generic import DetailView
 
-from .models import Movie
+from .models import Movie, Actor
 
 
 class MovieDetailView(DetailView):
     model = Movie
     template_name = "movies/movie_detail.html"
+
+
+class ActorDetailView(DetailView):
+    model = Actor
+    template_name = "movies/actor_detail.html"
