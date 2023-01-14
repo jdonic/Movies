2dfe1619836bad3273d8544a963e9f8c7f317d3a
jdonic <jdoni@jdonic.fedora>
2023-01-14 03:13:42 +0100

    Add search and improve styling
    Add url, view and template for search functionality
    Improve styling by creating static files

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,10 @@
 # movies/urls.py
 from django.urls import path
-from .views import MovieDetailView, ActorDetailView
+from .views import MovieDetailView, ActorDetailView, SearchListView
 
 
 urlpatterns = [
+    path("", SearchListView.as_view(), name="search_list"),
     path("movie_detail/<int:pk>/", MovieDetailView.as_view(), name="movie_detail"),
     path("actor_detail/<int:pk>/", ActorDetailView.as_view(), name="actor_detail"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
+
+from itertools import chain
 
 from .models import Movie, Actor
+from django.db.models import Q
 
 
 class MovieDetailView(DetailView):
@@ -11,3 +14,19 @@ class MovieDetailView(DetailView):
 class ActorDetailView(DetailView):
     model = Actor
     template_name = "movies/actor_detail.html"
+
+
+class SearchListView(ListView):
+    template_name = "movies/search_list.html"
+
+    # overwriting the get_queryset function to customize the queryset
+    def get_queryset(self) -> list:
+        query = self.request.GET.get("q")
+        if query:
+            movies = Movie.objects.filter(Q(title__icontains=query))
+            actors = Actor.objects.filter(Q(name__icontains=query))
+            # use the chain function to combine the querysets
+            return list(chain(movies, actors))
+        else:
+            # return an empty list if no query is provided
+            return []
